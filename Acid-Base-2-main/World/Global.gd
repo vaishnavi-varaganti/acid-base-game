e8e19d8adb3e1a09f625782fb2cb70a6a11103d0
vaishnavi varaganti <vaishnavivaraganti25@gmail.com>
2024-09-16 19:23:18 -0500

    displaying options in level 1

--- Acid-Base-2-main/World/Global.gd
+++ Acid-Base-2-main/World/Global.gd
@@ -1,11 +1,23 @@
 extends Node
 
 var current_level = 0
+var acidArray = []
+var baseArray = []
+var compoundArray = []
+var correct_answer = ""  # Variable to store the correct answer for each question
+var acidShooted = false
+var baseShooted = false 
+var level1Score = 0
+var level2Score = 0
+var level3Score = 0
+var level4Score = 0
+
+# URL for the API
+var api_url1 = "https://retoolapi.dev/Jqmkez/questions"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
