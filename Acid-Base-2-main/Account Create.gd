7c45c4e728af8653790d926b15db2b4a6c15efb4
vaishnavi varaganti <vaishnavivaraganti25@gmail.com>
2024-09-30 13:23:57 -0500

    ui modification in the create account page

--- Acid-Base-2-main/Account Create.gd
+++ Acid-Base-2-main/Account Create.gd
@@ -3,6 +3,7 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	$menu/Panel/PanelContainer/VBoxContainer2/status.visible = false
 	Firebase.Auth.signup_succeeded.connect(on_signup_succeeded)
 	Firebase.Auth.signup_failed.connect(on_signup_failed)
 
@@ -12,6 +13,7 @@ func _process(delta):
 	
 func on_signup_succeeded(auth):
 	print(auth)
+	$menu/Panel/PanelContainer/VBoxContainer2/status.visible = true
 	$menu/Panel/PanelContainer/VBoxContainer2/status.text = "Sign up success!"
 	Firebase.Auth.save_auth(auth)
 	get_tree().change_scene_to_file("res://Game.tscn")
@@ -20,16 +22,22 @@ func on_signup_succeeded(auth):
 func on_signup_failed(error_code, message):
 	print(error_code)
 	print(message)
+	$menu/Panel/PanelContainer/VBoxContainer2/status.visible = true
 	$menu/Panel/PanelContainer/VBoxContainer2/status.text = "Sign up failed. Error: %s" % message
 
 func _on_button_pressed():
-	var email = $menu/Panel/PanelContainer/VBoxContainer2/usernametext
-	var password = $menu/Panel/PanelContainer/VBoxContainer2/Passwordtext
+	$menu/Panel/PanelContainer/VBoxContainer2/status.visible = false
+	var email = $menu/Panel/PanelContainer/VBoxContainer2/UsernameContainer/usernametext
+	var password = $menu/Panel/PanelContainer/VBoxContainer2/PasswordContainer/Passwordtext
 	var user = email.text
 	var passwd = password.text
-	
 
 	Firebase.Auth.signup_with_email_and_password(user, passwd)
+	$menu/Panel/PanelContainer/VBoxContainer2/status.visible = true
 	$menu/Panel/PanelContainer/VBoxContainer2/status.text = "Singing up"
 	get_tree().change_scene_to_file("res://World/main_login.tscn")
 	pass # Replace with function body.
+	
+func _on_cancel_button_pressed():
+	get_tree().change_scene_to_file("res://World/main_login.tscn")
+	pass
