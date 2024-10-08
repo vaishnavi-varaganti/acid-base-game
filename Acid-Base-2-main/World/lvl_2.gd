8606e196aa2bdc8473d2b029d4983f4417ff6478
vaishnavi varaganti <vaishnavivaraganti25@gmail.com>
2024-10-07 19:22:34 -0500

    added sounds for correct and wrong answers in level 2

--- Acid-Base-2-main/World/lvl_2.gd
+++ Acid-Base-2-main/World/lvl_2.gd
@@ -28,7 +28,7 @@ func _ready():
 	Global.question_number = 1
 	print($hud/PanelContainer/HBoxContainer/Level.text)
 	$hud/PanelContainer/HBoxContainer/Level.text = "LEVEL " + str(Global.current_level)
-	http_request.request("https://retoolapi.dev/tnFVDY/acidsbases")
+	http_request.request("https://retoolapi.dev/tnFVDY/acidsbases$Panel/ButtonClickSound.play()")
 	connect_option_signals()
 	# Display options when the scene is loaded
 	display_options_level2()
@@ -104,6 +104,7 @@ func check_answer(selected_option: String):
 	if selected_option == Global.correct_answer:
 		print("Correct Answer!!!")
 		correct_popup.popup_centered()
+		$wrongPopup/Failure_Sound.play()
 		# Highlight correct answer visually (optional, e.g., change button color or text)
 		$hud/HBoxContainer/Option1.modulate = Color(0, 1, 0) if $hud/HBoxContainer/Option1.text == Global.correct_answer else Color(1, 1, 1)
 		$hud/HBoxContainer/Option2.modulate = Color(0, 1, 0) if $hud/HBoxContainer/Option2.text == Global.correct_answer else Color(1, 1, 1)
@@ -116,6 +117,7 @@ func check_answer(selected_option: String):
 	else:
 		print("Wrong Answer!")
 		wrong_popup.popup_centered()
+		$wrongPopup/Failure_Sound.play()
 		# Highlight the selected wrong answer in red
 		$hud/HBoxContainer/Option1.modulate = Color(1, 0, 0) if $hud/HBoxContainer/Option1.text == selected_option else Color(1, 1, 1)
 		$hud/HBoxContainer/Option2.modulate = Color(1, 0, 0) if $hud/HBoxContainer/Option2.text == selected_option else Color(1, 1, 1)
