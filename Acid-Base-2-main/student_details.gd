6daf7dbbd27f71d5ae3004c580b051c7a4b7ec42
AvinashChinnala1308 <avinashchinnala1325@gmail.com>
2024-10-08 15:01:01 -0500

    printing user details

--- Acid-Base-2-main/student_details.gd
+++ Acid-Base-2-main/student_details.gd
@@ -12,5 +12,11 @@ func _process(delta):
 
 
 func _on_button_pressed():
+	var first_name = $menu/Panel/PanelContainer/VBoxContainer2/Firstnamecontainer/firstnametext.text
+	var last_name = $menu/Panel/PanelContainer/VBoxContainer2/lastnamecontainer/LineEdit.text
+	var sid = $menu/Panel/PanelContainer/VBoxContainer2/sidcontainer/LineEdit.text
+
+	var gd = get_node("/root/Global")
+	gd.set_user_info(first_name, last_name, sid)
 	get_tree().change_scene_to_file("res://World/menu.tscn")
 	pass # Replace with function body.
