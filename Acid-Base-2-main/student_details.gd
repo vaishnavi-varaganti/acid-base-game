a312fe643011efcfb7c2cfad8113e6f5c8b80104
vaishnavi varaganti <vaishnavivaraganti25@gmail.com>
2024-10-08 16:34:33 -0500

    ui update in the student details page

--- Acid-Base-2-main/student_details.gd
+++ Acid-Base-2-main/student_details.gd
@@ -3,6 +3,7 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	$menu/Panel/PanelContainer/VBoxContainer2/Status.visible = false
 	pass # Replace with function body.
 
 
